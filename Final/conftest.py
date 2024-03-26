9724e03451e56753ed80e0783a842a621184ee26
LiSa <liz-fox.i@yandex.ru>
2024-03-26 21:16:20 +0300

    added new files

---
+++ Final/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+@pytest.fixture()
+def browser(scope="session"):
+   service = Service(executable_path='/bin/chromedriver/chromedriver')
+   options = webdriver.ChromeOptions()
+   driver = webdriver.Chrome(service=service, options=options)
+   yield driver
+   driver.quit()
+
